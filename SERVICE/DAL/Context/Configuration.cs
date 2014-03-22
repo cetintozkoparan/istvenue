92bd0b3e38e374e1f5be9df40d56ba56b01e7223
sakurahoshi <cetintozkoparan@hotmail.com>
2014-03-23 00:56:41 +0200

    fourth commit

---
+++ SERVICE/DAL/Context/Configuration.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.Migrations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DAL.Entities;
+
+namespace DAL.Context
+{
+    public class Configration : DbMigrationsConfiguration<MainContext>
+    {
+        public Configration()
+        {
+            AutomaticMigrationsEnabled = true; // production'da false yapılacak...
+            AutomaticMigrationDataLossAllowed = false;
+        }
+
+       
+    }
+}
---
+++ SERVICE/DAL/Migrations/201311212148386_InitialMigrations1.cs
@@ -0,0 +1,16 @@
+namespace DAL.Context
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class InitialMigrations1 : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
